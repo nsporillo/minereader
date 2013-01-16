040cbf5a4fca3768bd14d0581b49856d44708981
Nick Porillo <milkywayz@mail.com>
2013-01-15 19:18:55 -0500

    Fix plugin name

---
+++ src/net/minewriter/commands/CommandHandler.java
@@ -0,0 +1,52 @@
+package net.minewriter.commands;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.minewriter.MineReader;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author krinsdeath
+ */
+public class CommandHandler {
+
+	@SuppressWarnings("unused")
+	private final MineReader plugin;
+	private final Map<String, Command> commands = new HashMap<String, Command>();
+
+	public CommandHandler(final MineReader plugin) {
+		this.plugin = plugin;
+		this.commands.put("download", new DownloadCommand(plugin));
+	}
+
+	public void runCommand(final CommandSender sender, final String label,
+			final String[] args) {
+		if (args.length == 0
+				|| this.commands.get(args[0].toLowerCase()) == null) {
+			sender.sendMessage(ChatColor.GREEN + "===" + ChatColor.GOLD
+					+ " MineReader Help " + ChatColor.GREEN + "===");
+			for (final Command cmd : this.commands.values()) {
+				if (cmd.checkPermission(sender)) {
+					cmd.showHelp(sender, label);
+				}
+			}
+			return;
+		}
+		final List<String> arguments = new ArrayList<String>(
+				Arrays.asList(args));
+		final Command cmd = this.commands
+				.get(arguments.remove(0).toLowerCase());
+		if (arguments.size() < cmd.getRequiredArgs()) {
+			cmd.showHelp(sender, label);
+			return;
+		}
+		cmd.runCommand(sender, arguments);
+	}
+
+}
