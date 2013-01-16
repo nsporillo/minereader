f8368de323e25d76db5af1572f4ac8e7d9278813
Nick Porillo <milkywayz@mail.com>
2013-01-15 19:17:52 -0500

    We now cache books in the library =D

---
+++ src/net/minewriter/Library.java
@@ -0,0 +1,39 @@
+package net.minewriter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Library {	
+	private Set<Book> library = new HashSet<Book>();
+
+	public boolean add(Book b) {
+		return library.add(b);
+	}
+	
+	public boolean remove(Book b) {
+		return library.remove(b);
+	}
+	
+	public Book get(int id) {
+		for(Book b : library) {
+			if(b.getID() == id) {
+				return b;
+			}
+		}
+		return null;
+	}
+	
+	public Book get(String author, String title) {
+		for(Book b : library) {
+			if(b.getAuthor().equals(author) && b.getTitle().equals(title)) {
+				return b;
+			}
+		}
+		return null;
+	}
+	
+	
+	public Set<Book> getBooks() {
+		return library;
+	}
+}
